6bdc9127bd3c9c90975f58113e1a80bddc41de86
user <daniksurmach@gmail.com>
2020-02-06 13:55:00 +0200

    20200206_13_55

---
+++ darbi/diary_2020_02_06.py
@@ -0,0 +1,20 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("4")
+4
+>>> type('Hello, World!')
+<class 'str'>
+>>> type(17)
+<class 'int'>
+>>> type(3.2)
+<class 'float'>
+>>> type('17')
+<class 'str'>
+>>> type('3.2')
+<class 'str'>
+>>> print(1,000,000)
+1 0 0
+>>> print(1,00)
+1 0
+>>> 
